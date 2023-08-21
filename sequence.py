0eedc8b1e5ac50a9442a88442d9c793f1a6c93b2
giz86 <44810719+giz86@users.noreply.github.com>
2023-08-21 07:03:05 -0700

    import files from anothr file

--- sequence.py
+++ sequence.py
@@ -1,2 +1,29 @@
-gize = [1,5,8,12]
-print(gize[0:3])
+# gize = [1,5,8,12]
+# print(gize[0:3])
+#define  a list 
+# name = ["harry","aermione","ginny"]
+# from Demos.FileSecurityTest import name
+
+# name.append("gizealew")
+# name.sort()
+
+# print(name)
+# crete an enpity set
+# s =set()
+# s.add(3)
+# s.add(5)
+# s.add(12)
+# s.remove(5)
+# print(s)
+# len(s)
+# print(len(s))
+#loops
+# name = "harry"
+# for character in name:
+#     print(character)
+#dictionry
+# names = {"harry": "gryfindor","draco":"styler"}
+# print(names["draco"])
+import sequers 
+for i in range(10):
+    print(f"the square od {i} is the {sequers.square(i)}")
